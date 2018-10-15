3f96fca91dca4a5b2d4ce0378bc2f1eae922e6cc
Kevin Tjiam <kevin@kevincharm.com>
2018-10-15 19:00:27 +0200

    PoC sprite render

--- src/game/game.s
+++ src/game/game.s
@@ -25,6 +25,9 @@ along with gamelib-x64. If not, see <http://www.gnu.org/licenses/>.
 
 .section .game.data
 .equ VGA_MEM, 0xA0000
+.equ VGA_WIDTH, 320
+.equ VGA_HEIGHT, 200
+FMTSTR: .asciz "%u\n"
 
 .section .game.text
 
@@ -33,9 +36,37 @@ gameInit:
 
 	# test draw
 	mov $VGA_MEM, %rdi
-	movb $5, (%rdi)
-	movb $10, 4(%rdi)
-	movb $15, 8(%rdi)
+	mov $SHIP_VGA, %rsi
+	# ship width = 16
+	# ship height = 24
+
+	mov $VGA_WIDTH, %rdx
+	sub (SHIP_VGA_WIDTH), %rdx			# increment vga pointer by rdx after rendering a row
+
+	xor %r12, %r12
+loopy:
+	cmp (SHIP_VGA_HEIGHT), %r12
+	je loopy_end
+
+	xor %r13, %r13
+loopx:
+	cmp (SHIP_VGA_WIDTH), %r13
+	je loopx_end
+
+	mov (%rsi), %al
+	mov %al, (%rdi)
+	inc %rsi							# advance the sprite pointer as we consume
+	inc %rdi
+
+	inc %r13
+	jmp loopx
+loopx_end:
+	add %rdx, %rdi
+
+	inc %r12
+	jmp loopy
+loopy_end:
+
 
 	SUB_EPILOGUE
 	ret
