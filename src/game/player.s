b9cc8e03a68627905bf7e18db754d86ffcdfe7dd
Kevin Tjiam <kevin@kevincharm.com>
2018-10-15 20:02:34 +0200

    render sprite at offset

--- src/game/player.s
+++ src/game/player.s
@@ -1,37 +1,48 @@
 .include "src/game/lib/vga.s"
 
+.section .game.data
+.equ PLAYER_IDLE_OFFSET_X, (2 * 16)
+.equ PLAYER_HEIGHT, 24
+.equ PLAYER_WIDTH, 16
+
 # void draw_player(int x, int y);
 .global draw_player
 .type draw_player, @function
 draw_player:
     # Warning: no stackframe
-	mov $VGA_ADDR, %rdi
-	mov $SHIP_VGA, %rsi
-	# ship width = 16
-	# ship height = 24
+	mov $VGA_ADDR, %r8
+	mov $SHIP_VGA, %r9
 
 	mov $VGA_WIDTH, %rdx
-	sub (SHIP_VGA_WIDTH), %rdx			# increment vga pointer by rdx after rendering a row
+	sub $PLAYER_WIDTH, %rdx			    # increment vga pointer by rdx after rendering a row
+
+    mov (SHIP_VGA_WIDTH), %rcx
+    sub $PLAYER_WIDTH, %rcx
+    sub $PLAYER_IDLE_OFFSET_X, %rcx     # increment sprite pointer by rcx after rendering a row
+    mov $32, %rcx
 
 	xor %r12, %r12
 loopy:
-	cmp (SHIP_VGA_HEIGHT), %r12
+	cmp $PLAYER_HEIGHT, %r12
 	je loopy_end
 
+    add $PLAYER_IDLE_OFFSET_X, %r9         # advance sprite pointer by x offset per row
 	xor %r13, %r13
 loopx:
-	cmp (SHIP_VGA_WIDTH), %r13
+	cmp $PLAYER_WIDTH, %r13
 	je loopx_end
 
-	mov (%rsi), %al
-	mov %al, (%rdi)
-	inc %rsi							# advance the sprite pointer as we consume
-	inc %rdi
+    # When rendering a row, just copy & increment sequentially
+	mov (%r9), %al
+	mov %al, (%r8)
+    inc %r9
+	inc %r8
 
 	inc %r13
 	jmp loopx
 loopx_end:
-	add %rdx, %rdi
+	add %rdx, %r8                      # advance vga pointer by (row_width - player_width)
+    add %rcx, %r9                      # advance sprite pointer by (sheet_width - sprite_offset_x)
 
 	inc %r12
 	jmp loopy
